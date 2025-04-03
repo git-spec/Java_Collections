030002a9f4ec1d1ca1681eb8791a7073dff033ae
git-spec <inkyfischer@gmail.com>
2025-04-03 11:38:54 +0200

    feat: Added method printStudents to class School.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Student> students = new ArrayList(
+        List<Student> students = new ArrayList<>(
                 List.of(
                         new Student(
                                 123456,
@@ -25,14 +25,10 @@ public class Main {
                 )
         );
 
-        System.out.println("Students:");
-        for (Student student: students) {
-            System.out.println("ID: " + student.id);
-            System.out.println("Name: " + student.firstname + ' ' + student.lastname + "\n");
-        }
-
         for (Student student: students) {
             School.addStudent(student);
         }
+
+        School.printStudents();
     }
 }
--- src/main/java/org/example/School.java
+++ src/main/java/org/example/School.java
@@ -4,9 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class School {
-    static List<Student> students = new ArrayList();
+    static List<Student> students = new ArrayList<>();
 
     public static void addStudent(Student student) {
         students.add(student);
+    }
+
+    public static void printStudents() {
+        System.out.println("Students:");
+        for (Student student: students) {
+            System.out.println("ID: " + student.id);
+            System.out.println("Name: " + student.firstname + ' ' + student.lastname + "\n");
+        }
     }
 }
