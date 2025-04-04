7492ba0e4ed034f81162d7ce49c9bd285c2f5169
git-spec <inkyfischer@gmail.com>
2025-04-04 15:32:40 +0200

    feat: Created record Student.

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,13 +1,7 @@
 package org.example;
 
-public class Student {
-    int id;
-    String firstname;
-    String lastname;
-
-    public Student(int id, String firstname, String lastname) {
-        this.id = id;
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
-}
+public record Student(
+        int id,
+        String firstname,
+        String lastname
+        ) {}
