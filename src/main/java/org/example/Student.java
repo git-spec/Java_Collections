d8ab9e9df27953049d564b37ac7ff7001de9c8f9
git-spec <inkyfischer@gmail.com>
2025-04-05 17:10:41 +0200

    feat: Added method setCourse to Student.

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.List;
+
 public record Student(
         int id,
         String firstname,
-        String lastname
-        ) {}
+        String lastname,
+        List<Course> courses
+        ) {
+
+        public void setCourse(Course course) {
+                this.courses.add(course);
+        }
+}
