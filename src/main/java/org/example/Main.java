884748d657e93931feb65c6cfb396c8cb4c7a691
git-spec <inkyfischer@gmail.com>
2025-04-09 12:54:37 +0200

    feat: Created stream for file students.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,18 +16,21 @@ public class Main {
                                 123456,
                                 "Max",
                                 "Mustermann",
+                                20,
                                 new ArrayList<>()
                         ),
                         new Student(
                                 789101,
                                 "Erika",
                                 "Mustermann",
+                                19,
                                 new ArrayList<>()
                         ),
                         new Student(
                                 111213,
                                 "Jon",
                                 "Doe",
+                                22,
                                 new ArrayList<>()
                         )
                 )
@@ -72,20 +79,45 @@ public class Main {
         Telephone.add("John", 01618795463d);
         Telephone.add("Lisa", 01753495741d);
         Telephone.add("Gerald", 01671123655d);
-        Telephone.book.forEach( (k, v) -> { System.out.println(k + " -> " + v); } );
+        Telephone.book.forEach( (k, v) -> System.out.println(k + " -> " + v));
         System.out.println(' ');
         System.out.println(Telephone.getSize());
         System.out.println(' ');
         Telephone.deleteByName("John");
-        Telephone.book.forEach( (k, v) -> { System.out.println(k + " -> " + v); } );
+        Telephone.book.forEach( (k, v) -> System.out.println(k + " -> " + v));
         System.out.println(' ');
         Telephone.deleteAll();
-        Telephone.book.forEach( (k, v) -> { System.out.println(k + " -> " + v); } );
+        Telephone.book.forEach( (k, v) -> System.out.println(k + " -> " + v));
 
         Pharmacy pharmacy = new Pharmacy();
         pharmacy.save(new Medication("Thomapyrin Intensiv", 6.29, true));
         pharmacy.save(new Medication("Spalt", 11.45, false));
         pharmacy.save(new Medication("IBUPROFEN", 4.01, true));
         pharmacy.showAll();
+
+        /** STREAM **/
+        List<Student> result;
+        try (Stream<String> x = Files.lines(Path.of("students.csv"))) {
+            result = x
+                    .skip(1)
+                    .filter(s -> !s.isEmpty())
+                    .distinct()
+                    .map(s -> s.split(","))
+                    .map(s -> new Student(
+                            Integer.parseInt(s[0]),
+                            s[1],
+                            s[2],
+                            Integer.parseInt(s[3]),
+                            new ArrayList<>()
+                            )
+                        ).toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        result.forEach(s -> {
+            System.out.println("ID: " + s.id());
+            System.out.println("Name: " + s.firstname() + ' ' + s.lastname());
+            System.out.println("Age: " + s.age() + "\n");
+        });
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -6,6 +6,7 @@ public record Student(
         int id,
         String firstname,
         String lastname,
+        int age,
         List<Course> courses
         ) {
 
